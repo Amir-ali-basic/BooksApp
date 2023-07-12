57bf8445b541430ca3d87c7e8096efe7433144eb
Amir-ali-basic <alib.amir123@gmail.com>
2023-07-12 13:55:02 +0200

    Ping database Go

--- Beckend/cmd/api/main.go
+++ Beckend/cmd/api/main.go
@@ -5,43 +5,59 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"vue-api/internal/driver"
 )
 
+// config is the type for all application configuration
 type config struct {
-	port int
+	port int // what port do we want the web server to listen on
 }
 
+// application is the type for all data we want to share with the
+// various parts of our application. We will share this information in most
+// cases by using this type as the receiver for functions
 type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	db       *driver.DB
 }
 
+// main is the main entry point for our application
 func main() {
 	var cfg config
 	cfg.port = 8081
 
-	infoLog := log.New(os.Stdout, "Info\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stdout, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	dsn := "host=localhost port=5432 user=postgres password=password dbname=vueapi sslmode=disable timezone=UTC connect_timeout=5"
+	db, err := driver.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
 
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		db:       db,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// serve starts the web server
 func (app *application) serve() error {
-
 	app.infoLog.Println("API listening on port", app.config.port)
+
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", app.config.port),
 		Handler: app.routes(),
 	}
+
 	return srv.ListenAndServe()
 }
